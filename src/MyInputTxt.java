3758f01ad4b5664f0556ccef26f333fe0e047f2b
Albert <AccretionDiskThat@gmail.com>
2022-05-23 15:01:37 +0500

    create list save one method

--- src/MyInputTxt.java
+++ src/MyInputTxt.java
@@ -14,7 +14,6 @@ public class MyInputTxt {
         arr[num ? 1 : 0] = Integer.parseInt(reading);
     }
 
-
     public static int[] ReaderMethod() throws Exception {
         int[] arr = new int[2];
         BufferedReader br = new BufferedReader(new FileReader("src/input.txt"));
@@ -25,24 +24,9 @@ public class MyInputTxt {
         while (counter != 2 && (str = br.readLine()) != null) {
             int length = str.length();
             if (str.charAt(0) == 'a') {
-                //String reading = "";
                 FillArrayx(str, arr,false);
-//                for (int i = 1; i < length; i++) {
-//                    if (Character.isDigit(str.charAt(i))) {
-//                        reading += Character.toString(str.charAt(i));
-//                    }
-//                }
-                //arr[0] = Integer.parseInt(reading);
             } else if (str.charAt(0) == 'b') {
                 FillArrayx(str, arr,true);
-
-//                String reading = "";
-//                for (int i = 1; i < length; i++) {
-//                    if (Character.isDigit(str.charAt(i))) {
-//                        reading += Character.toString(str.charAt(i));
-//                    }
-//                }
-//                arr[1] = Integer.parseInt(reading);
             }
         counter++;
         }
